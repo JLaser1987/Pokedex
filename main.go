bb42aa647ddaa37dfb8876140ef391596ed70483
Lasolimu <Lasolimu@gmail.com>
2026-06-12 13:44:19 -0600

    add help and exit commands to the pokedex.

--- main.go
+++ main.go
@@ -1,20 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"bufio"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for {
-		fmt.Print("Pokedex > ")
-		if scanner.Scan() {
-			input := scanner.Text()
-			words := cleanInput(input)
-			fmt.Printf("Your command was: %s\n", words[0])
-		}
-	}
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,6 +1,33 @@
 package main
 
-import "strings"
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		if scanner.Scan() {
+			input := scanner.Text()
+			words := cleanInput(input)
+
+			command, exists := getCommands()[words[0]]
+			if exists {
+				err := command.callback()
+				if err != nil {
+					fmt.Println(err)
+				}
+			} else {
+				fmt.Println("Unknown command")
+			}
+		}
+	}
+}
 
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
